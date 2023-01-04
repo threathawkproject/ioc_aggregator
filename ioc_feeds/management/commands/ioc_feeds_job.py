d41c84353e0eb4fbfa2da5ac3345fa0a2a49059d
zohaibadnan137 <zohaibadnan137@gmail.com>
2023-01-04 18:43:55 +0500

    Add AbuseIPDB

--- ioc_feeds/management/commands/ioc_feeds_job.py
+++ ioc_feeds/management/commands/ioc_feeds_job.py
@@ -6,55 +6,109 @@ import requests
 from ioc_feeds.producer import KafkaProducerWrapper
 import time
 
-
-# Third party
+# Third party libraries
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
-# this is how you fetch the ioc feeds
+
+
+# Each function fetches indicators from a different source
 def fetch_darklist():
-    print("Fetching from darklist...")
+    print("Fetching indicators from Darklist...")
+
     try:
         response = requests.get("https://darklist.de/raw.php")
         response.raise_for_status()
+
         ip_addresses = response.text.strip().split()[11:]
-        iocs = list(map(lambda ip : {
-                "ioc": ip,
-                "type": "IP",
-                "source": "darklist"
-            },
+        iocs = list(map(lambda ip: {
+            "ioc": ip,
+            "type": "IP",
+            "source": "Darklist"
+        },
             ip_addresses
         ))
         return iocs
+
     except Exception as e:
         print(f"Error occured: {e}")
         return []
 
+
 def fetch_blocklist():
-    print("Fetching from blocklist...")
+    print("Fetching indicators from Blocklist...")
+
+    try:
+        response = requests.get("https://lists.blocklist.de/lists/all.txt")
+        response.raise_for_status()
+
+        ip_addresses = response.text.strip().split("\n")
+        iocs = list(map(lambda ip: {
+            "ioc": ip,
+            "type": "IP",
+            "source": "Blocklist"
+        },
+            ip_addresses
+        ))
+        return iocs
+
+    except Exception as e:
+        print(f"Error")
+        return []
+
 
 def fetch_abuseIPDB():
-    print("Fetching from abuse IP DB...")
+    print("Fetching indicators from AbuseIPDB...")
+
+    try:
+        headers = {"Key": "aa08332f175a5c5b3ef2c4606197aad7d7a83df1a68b119c302ba6cd9156f7f097f8757b738bb840",
+                   "Accept": "application/json"}
+        response = requests.get(
+            "https://api.abuseipdb.com/api/v2/blacklist", headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+        iocs = list(map(lambda element: {
+            "ioc": element["ipAddress"],
+            "type": "IP",
+            "source": "AbuseIPDB",
+            "location": element["countryCode"]
+        },
+            data["data"]
+        ))
+        return iocs
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
 
 def fetch_botvrij():
-    print("Fetching from botvrij...")
+    print("Fetching from Botvrij...")
+
     try:
-        response = requests.get("https://www.botvrij.eu/data/ioclist.email-src")
+        response = requests.get(
+            "https://www.botvrij.eu/data/ioclist.email-src")
         response.raise_for_status()
+
         values = response.text.strip().split("\n")[6:]
         emails = list(
             map(lambda sentence: {
-                    "ioc": sentence.strip().split(" ")[0],
-                    "type": "email",
-                    "source": "botvrij"
-                }, 
+                "ioc": sentence.strip().split(" ")[0],
+                "type": "Email",
+                "source": "Botvrij"
+            },
                 values
             )
         )
         return emails
+
     except Exception as e:
+        print(f"Error: {e}")
         return []
+
+
 def publish(iocs):
     try:
         producer = KafkaProducerWrapper.instance()
@@ -62,18 +116,20 @@ def publish(iocs):
             producer.send("ioc_feed", ioc)
             # time.sleep(1)
     except Exception as e:
-        print(f"Error occured: {e}")
+        print(f"Error: {e}")
+
 
 class run_aggregator():
-        ioc_feed_respone = []
-        botvrij_iocs = fetch_botvrij()
-        darklist_iocs = fetch_darklist()
-        if len(botvrij_iocs) > 0:
-            ioc_feed_respone.extend(botvrij_iocs)
-        if len(darklist_iocs) > 0:
-            ioc_feed_respone.extend(darklist_iocs)
-        # print(ioc_feed_respone)
-        publish(ioc_feed_respone)
+    ioc_feed_respone = []
+    botvrij_iocs = fetch_botvrij()
+    darklist_iocs = fetch_darklist()
+    if len(botvrij_iocs) > 0:
+        ioc_feed_respone.extend(botvrij_iocs)
+    if len(darklist_iocs) > 0:
+        ioc_feed_respone.extend(darklist_iocs)
+    # print(ioc_feed_respone)
+    publish(ioc_feed_respone)
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -98,7 +154,6 @@ class Command(BaseCommand):
         #     scheduler.shutdown()
         #     print("Scheduler shut down successfully!")
 
-
         # ioc_feed_respone = []
         # botvrij_iocs = fetch_botvrij()
         # darklist_iocs = fetch_darklist()
@@ -106,7 +161,7 @@ class Command(BaseCommand):
         #     ioc_feed_respone.extend(botvrij_iocs)
         # if len(darklist_iocs) > 0:
         #     ioc_feed_respone.extend(darklist_iocs)
-        
+
         # print(ioc_feed_respone)
 
         # print(ioc_feed_respone)
